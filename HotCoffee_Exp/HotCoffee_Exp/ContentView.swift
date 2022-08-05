1632b5407aec56c4999ae4edef3182516748fd98
Shak49 <shakfeizi@gmali.com>
2022-08-05 10:37:36 -0500

    project's files added

---
+++ HotCoffee_Exp/HotCoffee_Exp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  HotCoffee_Exp
+//
+//  Created by Shak Feizi on 8/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotCoffee_Exp/HotCoffee_Exp/HotCoffee_ExpApp.swift
@@ -0,0 +1,20 @@
+//
+//  HotCoffee_ExpApp.swift
+//  HotCoffee_Exp
+//
+//  Created by Shak Feizi on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct HotCoffee_ExpApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
